60210e21d85c4f9bc0e9b180f5b91cb73c76b1bc
Vernon Andrade <andradevern@gmail.com>
2020-11-11 18:00:31 -0500

    initial commit

---
+++ popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('destroy');
+  checkPageButton.addEventListener('click', function() {
+    var text = document.getElementById('url').value;
+  }, false);
+}, false);
