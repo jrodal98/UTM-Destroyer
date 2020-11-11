e09a64ff9a4b4e276f7f84a87a909fc7cd496804
Jacob Rodal <jrodal98@gmail.com>
2020-11-11 18:07:29 -0500

    detect if a url is valid

--- popup.js
+++ popup.js
@@ -1,6 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('destroy');
-  checkPageButton.addEventListener('click', function() {
-    var text = document.getElementById('url').value;
-  }, false);
+    var checkPageButton = document.getElementById('destroy');
+    checkPageButton.addEventListener('click', function() {
+        var text = document.getElementById('url').value;
+        // https://stackoverflow.com/a/5717133/9063770
+        var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+            '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+            '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+            '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+        if (pattern.test(text)) {
+            alert("Valid URL");
+        } else {
+            alert("Invalid URL")
+        }
+    }, false);
 }, false);
